b257149c2bcd36adca55bf6f7473301ed134fe2d
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2015-10-26 17:09:59 +0100

    chore(GroceryService): Factor out Hello World routes

--- spark/src/main/java/bentolor/grocerylist/GroceryService.java
+++ spark/src/main/java/bentolor/grocerylist/GroceryService.java
@@ -1,38 +1,13 @@
 package bentolor.grocerylist;
 
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.template.jade.JadeTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static spark.Spark.*;
+import static spark.Spark.port;
 
 public class GroceryService {
 
     public static void main(String[] args) {
-
         port(8080);
 
-        get("/hello", "text/html", (req, res) -> "<!DOCTYPE html><html><h1>Hello World</h1></html>");
-        get("/hello", (req, res) -> "Hello World");
 
-        get("/env/:varname", ((req, res) -> System.getenv(req.params("varname"))));
-
-        get("/greet", "text/html", (req, res) -> {
-            Map<String,String> values = new HashMap<>();
-            String name = req.queryParams("name");
-            values.put("name", name != null ? name : "Anon");
-            return new ModelAndView(values, "greet");
-        }, new JadeTemplateEngine());
-
-        get("/protected", GroceryService::unauthorized);
     }
 
-    public static Object unauthorized(Request request, Response response) throws Exception {
-        halt(200, "Go away!");
-        return null;
-    }
 }
---
+++ spark/src/main/java/bentolor/grocerylist/HelloWorldService.java
@@ -0,0 +1,40 @@
+package bentolor.grocerylist;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.jade.JadeTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class HelloWorldService {
+
+  public static void main(String[] args) {
+    port(8080);
+
+    get("/hello", "text/html", (req, res)
+      -> "<!DOCTYPE html><html><h1>" +
+         "Hello World</h1></html>");
+    get("/hello", (req, res) -> "Hello World");
+
+    get("/env/:varname", ((req, res)
+      -> System.getenv(req.params("varname"))));
+
+    get("/greet", "text/html", (req, res) -> {
+      Map<String, String> values = new HashMap<>();
+      String n = req.queryParams("name");
+      values.put("name", n != null ? n : "Anon");
+      return new ModelAndView(values, "greet");
+    }, new JadeTemplateEngine());
+
+    get("/protected", HelloWorldService::unauthorized);
+  }
+
+  public static Object unauthorized(Request request, Response response) throws Exception {
+    halt(200, "Go away!");
+    return null;
+  }
+}
