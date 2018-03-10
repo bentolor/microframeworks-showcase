177fd893969c34c7db6c4006f7924ddaf8cdea52
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2018-03-10 16:27:03 +0100

    refactor(Ninja): replace method strings with Java8 method references

--- ninja/src/main/java/conf/Routes.java
+++ ninja/src/main/java/conf/Routes.java
@@ -26,18 +26,18 @@ public class Routes implements ApplicationRoutes {
     @Override
     public void init(Router router) {
         // Hello world example with I18N
-        router.GET().route("/hello").with(HelloWorldController.class, "helloWorld");
+        router.GET().route("/hello").with(HelloWorldController::helloWorld);
 
         // Grocery List application routes
-        router.GET().route("/").with(GroceryListController.class, "index");
-        router.GET().route("/list").with(GroceryListController.class, "getAllGroceryLists");
-        router.GET().route("/list/{id: \\w+-\\w+-\\w+-\\w+-\\w+}").with(GroceryListController.class, "getGroceryList");
-        router.DELETE().route("/list/{id}").with(GroceryListController.class, "deleteGroceryList");
-        router.POST().route("/list").with(GroceryListController.class, "createGroceryList");
-        router.PUT().route("/list/{id}").with(GroceryListController.class, "updateGroceryList");
+        router.GET().route("/").with(GroceryListController::index);
+        router.GET().route("/list").with(GroceryListController::getAllGroceryLists);
+        router.GET().route("/list/{id: \\w+-\\w+-\\w+-\\w+-\\w+}").with(GroceryListController::getGroceryList);
+        router.DELETE().route("/list/{id}").with(GroceryListController::deleteGroceryList);
+        router.POST().route("/list").with(GroceryListController::createGroceryList);
+        router.PUT().route("/list/{id}").with(GroceryListController::updateGroceryList);
 
         // Assets (pictures / javascript)
-        router.GET().route("/assets/{fileName: .*}").with(AssetsController.class, "serveStatic");
+        router.GET().route("/assets/{fileName: .*}").with(AssetsController::serveStatic);
     }
 
 }
